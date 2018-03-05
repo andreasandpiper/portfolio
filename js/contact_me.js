ced0fb1f83f7aa36d166fb2d7d69bd3ccba9f6da
Andrea <andrea.wayte@gmail.com>
2018-03-05 10:10:37 -0800

    form validate, trim white space and add loading btn while sends

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,65 +1,79 @@
-$(function() {
+$(function () {
 
- $("input,textarea").jqBootstrapValidation(
-    {
-     preventSubmit: true,
-     submitError: function($form, event, errors) {
+  $("input,textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function ($form, event, errors) {
       // something to have when submit produces an error ?
-     },
-     submitSuccess: function($form, event) {
+    },
+    submitSuccess: function ($form, event) {
       event.preventDefault(); // prevent default submit behaviour
-       // get values from FORM
-       var name = $("input#name").val();  
-       var email = $("input#email").val(); 
-       var message = $("textarea#message").val();
-        var firstName = name; // For Success/Failure Message
-           // Check for white space in name for Success/Fail message
-        if (firstName.indexOf(' ') >= 0) {
-	   firstName = name.split(' ').slice(0, -1).join(' ');
-         }        
-	 $.ajax({
-                url: "php_mailer/mail_handler.php",
-            	type: "POST",
-            	data: {name: name, email: email, message: message},
-            	cache: false,
-            	success: function() {  
-            	// Success message
-            	   $('#success').html("<div class='alert alert-success'>");
-            	   $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            		.append( "</button>");
-            	  $('#success > .alert-success')
-            		.append("<strong>Your message has been sent. </strong>");
- 		  $('#success > .alert-success')
- 			.append('</div>');
- 						    
- 		  //clear all fields
- 		  $('#contactForm').trigger("reset");
- 	      },
- 	   error: function() {		
- 		// Fail message
- 		 $('#success').html("<div class='alert alert-danger'>");
-            	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
- 	        $('#success > .alert-danger').append('</div>');
- 		//clear all fields
- 		$('#contactForm').trigger("reset");
- 	    },
-           })
-         },
-         filter: function() {
-                   return $(this).is(":visible");
-         },
-       });
+      // get values from FORM
+      $('.submit-message').html("<i class=\"fa fa-spinner fa-spin\" style=\"font-size:24px\"></i>")
+      var name = $("input#name").val().trim();
+      var email = $("input#email").val().trim();
+      var message = $("textarea#message").val().trim();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
 
-      $("a[data-toggle=\"tab\"]").click(function(e) {
-                    e.preventDefault();
-                    $(this).tab("show");
-        });
-  });
- 
 
-/*When clicking on Full hide fail/success boxes */ 
-$('#name').focus(function() {
-     $('#success').html('');
+      $.ajax({
+        url: "php_mailer/mail_handler.php",
+        type: "POST",
+        data: {
+          name: name,
+          email: email,
+          message: message
+        },
+        cache: false,
+        success: function (resp) {
+          resp = JSON.parse(resp)
+          if (resp.success) {
+            // Success message
+            $('#success').html("<div class='alert alert-success'>");
+            $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+              .append("</button>");
+            $('#success > .alert-success')
+              .append("<strong>Your message has been sent. </strong>");
+            $('#success > .alert-success')
+              .append('</div>');
+
+            //clear all fields
+            $('#contactForm').trigger("reset");
+
+          }
+          $('.submit-message').text("Submit Message")
+
+        },
+        error: function (err) {
+          console.log(err)
+
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+            .append("</button>");
+          $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
+          $('#success > .alert-danger').append('</div>');
+          //clear all fields
+          $('#contactForm').trigger("reset");
+
+          $('.submit-message').text("Submit Message")
+
+        },
+      })
+    },
+    filter: function () {
+      return $(this).is(":visible");
+    },
   });
+
+  $("a[data-toggle=\"tab\"]").click(function (e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function () {
+  $('#success').html('');
+});
